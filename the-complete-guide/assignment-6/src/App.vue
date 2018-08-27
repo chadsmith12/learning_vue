5535a51cfcd7c2155f1dd3d5209902dc76006e60
Chad Smith <chadsmith4@my.unt.edu>
2018-08-27 00:35:32 -0500

    added assignment  6 to the complete guide to vue. Broke into components

---
+++ the-complete-guide/assignment-6/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <app-servers></app-servers>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import AppServers from "./components/AppServers.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+    components: {
+        'app-header': Header,
+        'app-servers': AppServers,
+        'app-footer': Footer
+    }
+}
+
+</script>
+
+<style>
+
+</style>
