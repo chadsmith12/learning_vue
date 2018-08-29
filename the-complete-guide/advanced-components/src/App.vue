2d1abc1284d4c122918159a31a5de7cd7f98d26e
Chad Smith <chadsmith4@my.unt.edu>
2018-08-28 23:00:45 -0500

    added initial project for advanced-components example

---
+++ the-complete-guide/advanced-components/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12">
+          <Quote/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Quote,
+  },
+};
+</script>
+
