5b5c5f578f3ab147e333016c5625a95721378f2e
Chad Smith <chadsmith4@my.unt.edu>
2018-08-29 00:09:01 -0500

    added solution to assignment-8 to show how to use dynamic components

---
+++ the-complete-guide/assignment-8/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <br>
+                <button @click="selectedTemplate = 'Blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedTemplate = 'Green'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedTemplate = 'Red'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="selectedTemplate">
+                  The Template!
+                </component>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blue from './components/Blue.vue'
+import Green from './components/Green.vue'
+import Red from './components/Red.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Blue,
+    Green,
+    Red
+  },
+  data() {
+    return {
+      selectedTemplate: 'Blue'
+    }
+  }
+}
+</script>
