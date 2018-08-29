8046ada3814f182054bc7062941c7e41ab88c5b4
Chad Smith <chadsmith4@my.unt.edu>
2018-08-29 00:47:50 -0500

    add quote grid and quote component

--- the-complete-guide/quotes-app/src/App.vue
+++ the-complete-guide/quotes-app/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app" class="container">
-
+    <QuoteGrid :quotes="quotes"/>
   </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
 
 export default {
+  data() {
+    return {
+      maxQuotes: 10,
+      quotes: ['Just a quote']
+    }
+  },
+  components: {
+    QuoteGrid
+  }
 }
 </script>
---
+++ the-complete-guide/quotes-app/src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <Quote v-for="(quote, index) in quotes" :key="index">
+            {{ quote }}
+        </Quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+    name: 'QuoteGrid',
+    props: ['quotes'],
+    components: {
+        Quote
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
