81903d4568f8f08cdf54417d674520ecf89d8b3b
Chad Smith <chadsmith4@my.unt.edu>
2018-08-31 00:30:59 -0500

    completed assignment 9 of the complete guide to Vue. Demonstrating forms.

---
+++ the-complete-guide/assignment-9/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                     <div class="form-group">
+                        <label for="fullname">Full Name</label>
+                        <FullName
+                                v-model="fullName"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input
+                                type="email"
+                                id="email"
+                                class="form-control"
+                                v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="sendData">
+                            <input
+                                    type="checkbox"
+                                    id="sendData"
+                                    v-model="sendData"> Send Data
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                            <button
+                                    class="btn btn-primary"
+                                    @click.prevent="submitForm">Submit!
+                            </button>
+                        </div>
+                    </div>
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ sendData }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import FullName from "./components/FullName.vue";
+ 
+    export default {
+        data() {
+            return {
+                fullName: "",
+                email: "",
+                password: "",
+                sendData: false,
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitForm() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            FullName
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ the-complete-guide/assignment-9/src/components/FullName.vue
@@ -0,0 +1,27 @@
+<template>
+    <input
+        type="text"
+        id="fullname"
+        class="form-control"
+        :value="value"
+        @input="changedInput">
+</template>
+
+<script>
+export default {
+    name: 'FullName',
+    props: ['value'],
+    data() {
+        return {
+            currentValue: this.value
+        }  
+    },
+    methods: {
+        changedInput(event) {
+            this.currentValue = event.target.value;
+            this.$emit('input', this.currentValue);
+        }
+    }
+}
+</script>
+
