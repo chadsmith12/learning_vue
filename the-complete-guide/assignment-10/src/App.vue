9d47035187826ad5f44a066d0022f1b3ae20a8df
Chad Smith <chadsmith4@my.unt.edu>
2018-09-02 01:34:58 -0500

    added assignment-10, creating custom event directive

---
+++ the-complete-guide/assignment-10/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <input type="text" v-app-on:input="onInput">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppOn from "./directives/AppOn.vue";
+    export default {
+        data() {
+            return {
+                currentValue: ''
+            }
+        },
+        methods: {
+            onInput(event) {
+                event.target.value = event.target.value.toUpperCase();
+            }
+        },
+        directives: {
+            'app-on': AppOn
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ the-complete-guide/assignment-10/src/directives/AppOn.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+    bind(el, binding, vNode) {
+        let eventType = binding.arg;
+        el.addEventListener(eventType, binding.value);
+    }
+}
+</script>
