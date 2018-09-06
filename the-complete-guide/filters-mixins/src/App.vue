889b614bc3f7be70fbed063bd6ffb3e1c1f88f95
Chad Smith <chadsmith4@my.unt.edu>
2018-09-06 00:25:23 -0500

    created filter-mixins base code

---
+++ the-complete-guide/filters-mixins/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
