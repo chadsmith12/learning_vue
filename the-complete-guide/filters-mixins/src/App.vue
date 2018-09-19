c78f0a0772bbdb6ba0b07e50066fd361539470c8
Chad Smith <chadsmith4@my.unt.edu>
2018-09-18 23:56:48 -0500

    added example of working with a local and global filter

--- the-complete-guide/filters-mixins/src/App.vue
+++ the-complete-guide/filters-mixins/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <div class="container">
-      <img alt="Vue logo" src="./assets/logo.png">
+      <p>{{ text | toUppercase | to-lowercase }}</p>
+      <hr>
+      <input type="text" v-model="filterText">
+      <ul>
+        <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -9,5 +14,24 @@
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      text: 'Hello from the other side!',
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    }
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      })
+    }
+  }
 }
 </script>
