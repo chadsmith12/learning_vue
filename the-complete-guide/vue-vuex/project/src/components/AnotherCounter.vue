2d957f9f60e68a6c5f351d4b557fb11397ab9c39
Chad Smith <chadsmith4@my.unt.edu>
2018-11-03 00:49:40 -0500

    adding examples of working with mutations and actions

---
+++ the-complete-guide/vue-vuex/project/src/components/AnotherCounter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 100, duration: 500})">Async Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 100, duration: 500})">Async Decrement</button>
+    </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+    export default {
+        methods: {
+            ...mapActions(['asyncIncrement', 'asyncDecrement'])
+        }
+    }
+</script>
--- the-complete-guide/vue-vuex/project/src/store/store.js
+++ the-complete-guide/vue-vuex/project/src/store/store.js
@@ -10,6 +10,35 @@ export const store = new Vuex.Store({
     getters: {
         doubleCounter: (state) => {
             return state.counter * 2;
+        },
+        stringCounter: (state) => {
+            return state.counter + " clicks";
+        }
+    },
+    mutations: {
+        increment: (state, payload) => {
+            state.counter+=payload;
+        },
+        decrement: (state, payload) => {
+            state.counter-=payload;
+        }
+    },
+    actions: {
+        increment: (context, payload) => {
+            context.commit('increment', payload);
+        },
+        decrement: (context, payload) =>{
+            context.commit('decrement', payload);
+        },
+        asyncIncrement: (context, payload) => {
+            setTimeout(() => {
+                context.commit('increment', payload.by)
+            }, payload.duration);
+        },
+        asyncDecrement: (context, payload) => {
+            setTimeout(() => {
+                context.commit('decrement', payload.by)
+            }, payload.duration)
         }
     }
 })
